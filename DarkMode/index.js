1f31c25dfe7b831aa6fee53c590ba1e729b34eff
2heeesss <hohooodo@gmail.com>
2021-11-30 11:30:30 +0900

    Feat: add darkmode ui component

---
+++ DarkMode/index.js
@@ -0,0 +1,28 @@
+(function () {
+  setTimeout(() => {
+    document.body.style.visibility = 'visible';
+  }, 300);
+
+  function toggleDarkMode(current) {
+    current === 'dark' ? document.body.classList.add('dark') : document.body.classList.remove('dark');
+  }
+
+  let currentMode = window.localStorage.getItem('currentMode');
+
+  const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  if (darkModeMediaQuery.matches) currentMode = 'dark';
+
+  darkModeMediaQuery.addEventListener('change', e => {
+    currentMode = e.matches ? 'dark' : 'light';
+    toggleDarkMode(currentMode);
+  });
+
+  toggleDarkMode(currentMode);
+
+  const $toggleBtn = document.querySelector('.toggle-button');
+  $toggleBtn.addEventListener('click', () => {
+    currentMode === 'dark' ? document.body.classList.remove('dark') : document.body.classList.add('dark');
+    currentMode = currentMode === 'dark' ? 'light' : 'dark';
+    window.localStorage.setItem('currentMode', currentMode);
+  });
+})();
